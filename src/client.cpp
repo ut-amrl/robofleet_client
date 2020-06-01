1dd58de6042c4501e3cf83ef120284c0988cf999
Logan Zartman <logan.zartman@utexas.edu>
2020-06-01 15:49:32 -0500

    add flexbuffers, test message encode

--- src/client.cpp
+++ src/client.cpp
@@ -1,13 +1,26 @@
+#include <cstdint>
 #include <iostream>
 #include <string>
+#include <vector>
 
+#include <flatbuffers/flexbuffers.h>
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 
 const static std::string node_name = "robofleet_client";
 
+std::vector<uint8_t> encode(const std_msgs::String& msg) {
+    flexbuffers::Builder fbb;
+    fbb.Map([&](){
+        fbb.String("data", msg.data);
+    });
+    fbb.Finish();
+    return fbb.GetBuffer();
+}
+
 void test_callback(const std_msgs::String& msg) {
-    std::cout << "Sending message: " << msg.data << std::endl;
+    std::vector<uint8_t> buffer = encode(msg);
+    std::cout << "Sending message length: " << buffer.size() << std::endl;
 }
 
 int main(int argc, char** argv) {
