b4a04c973fdf5fe4d25983a4cd5104d3becf0e6e
Logan Zartman <logan.zartman@utexas.edu>
2020-06-04 02:02:02 -0500

    flatbuffer example instead of flexbuffer

--- src/encoding.hpp
+++ src/encoding.hpp
@@ -1,23 +1,27 @@
 #pragma once
 
-#include <flatbuffers/flexbuffers.h>
+#include <algorithm>
+#include <flatbuffers/flatbuffers.h>
 #include <std_msgs/String.h>
+#include <std_msgs_string_generated.h>
 
 #include <vector>
 
 static std::vector<uint8_t> encode(const std_msgs::String& msg) {
-  flexbuffers::Builder fbb;
-  fbb.Map([&]() {
-    fbb.String("data", msg.data);
-  });
-  fbb.Finish();
-  return fbb.GetBuffer();
+  StdMsgsStringT obj;
+  obj.data = msg.data;
+  flatbuffers::FlatBufferBuilder fbb;
+  fbb.Finish(StdMsgsString::Pack(fbb, &obj));
+
+  std::vector<uint8_t> data(fbb.GetSize());
+  std::copy(fbb.GetBufferPointer(), fbb.GetBufferPointer() + fbb.GetSize(), data.begin());
+  return data;
 }
 
 static std_msgs::String decode_string(const uint8_t* const data,
                                       const size_t& size) {
-  flexbuffers::Map m = flexbuffers::GetRoot(data, size).AsMap();
+  const StdMsgsString* str = GetStdMsgsString(data);
   std_msgs::String msg;
-  msg.data = m["data"].AsString().str();
+  msg.data = str->data()->str();
   return msg;
 }
